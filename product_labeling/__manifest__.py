6b919ccc572208b0c9115789b34538b5745ca78e
default <mosiskomunia@gmail.com>
2023-12-16 10:02:31 -0500

    Создал отображения для добавления товара и добавления акта изменения свойств товара. А также добавил модуль в меню и создал действия для обоих подпунктов меню

--- product_labeling/__manifest__.py
+++ product_labeling/__manifest__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "product_labeling",
+    'name': "Маркировка Товара",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Тестовое задание для BNPI bot",
 
     'description': """
-Long description of module's purpose
+    О этот прекрасный модуль передает всю суть Odoo фреймворк и небольшой спектр эмоций, которые я испытал пытаясь понять как это работает :)
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "default",
+    'website': "https://google.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
--- product_labeling/models/models.py
+++ product_labeling/models/models.py
@@ -1,22 +1,23 @@
 from odoo import models, fields, api
 
-
+#Модель Товара
 class Product(models.Model):
     _name = "product.model"
 
     name = fields.Char(required=True)
     description = fields.Text(required=True)
 
+#Модель акта для изменения свойств товара
 class Properties(models.Model):
     _name = "properties.properties"
 
-    status = fields.Selection(selection = lambda self: self.method())
+    status = fields.Selection(selection =  lambda self: self._select_choise()) #[("продажа", "Продажа"),("покупка","Покупка")])
     stock_from = fields.Char(required = True)
     stock_where = fields.Char(required = True)
-    product = fields.Many2one(Product, ondelete = 'no action')
+    product = fields.Many2one('product.model', string="Product", ondelete = 'no action')
     count = fields.Integer(required = True)
 
     @api.model
     def _select_choise(self):
-        select_list = ["Продажа", "Покупка"]
+        select_list = [("продажа","Продажа"),("покупка","Покупка")]
         return select_list
